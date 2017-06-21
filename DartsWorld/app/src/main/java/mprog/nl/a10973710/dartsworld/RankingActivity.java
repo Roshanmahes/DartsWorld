4c37144307eb2e3864cbfaf52d5f05d0a888a4ac
Roshanmahes <roshan-1@live.com>
2017-06-21 15:07:47 +0100

    Test

--- DartsWorld/app/src/main/java/mprog/nl/a10973710/dartsworld/RankingActivity.java
+++ DartsWorld/app/src/main/java/mprog/nl/a10973710/dartsworld/RankingActivity.java
@@ -60,13 +60,6 @@ public class RankingActivity extends AppCompatActivity
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.ranking, menu);
-        return true;
-    }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
